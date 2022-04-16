af8a1755f879ae6ced094388f20c4da9257346b2
Daniil4949 <91974808+Daniil4949@users.noreply.github.com>
2022-04-16 14:05:51 +0300

    Lab 3 OAIP(sem 2)

---
+++ LabOAIP3/lab3.sem2/lab3.sem2.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<cmath>
+#include<stack>
+using namespace std;
+int A(int m, int n) {
+	if (m == 0) {
+		return n + 1;
+	}
+	else if (m > 0 && n == 0) {
+		return A(m - 1, 1);
+	}
+	return A(m - 1, A(m, n - 1));
+}
+int main() {
+	system("chcp 1251>nul");
+	int n;
+	int m;
+	int result = 0;
+	cout << "Введите значение m:" << endl;
+	cin >> m;
+	cout << "Введите значение n:" << endl;
+	cin >> n;
+	cout << "Значение функции Аккермана: " << endl;
+	cout<<A(m,n)<<endl;
+	
+	return 0;
+}
